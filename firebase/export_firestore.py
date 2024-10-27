86861a609e322825f8a044e0e200b0858eaea7d1
ryuheisato <ryuheisato7@gmail.com>
2024-10-26 18:56:49 -1000

    firestoreエクスポートの列名変更

--- firebase/export_firestore.py
+++ firebase/export_firestore.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Firebase Admin SDKの初期化
-cred = credentials.Certificate('./ai-finder-4a04e-firebase-adminsdk-f1lm9-5aac28c1c0.json')
+cred = credentials.Certificate('../ai-finder-4a04e-firebase-adminsdk-f1lm9-5aac28c1c0.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -15,7 +15,7 @@ docs = db.collection(collection_name).stream()
 csv_file_path = 'exported_firestore_data.csv'
 
 with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['DocumentID', 'Name', 'Tagline', 'Description', 'Votes Count', 'Website', 'Topic Names', 'Reviews Rating', 'Slug', 'Thumbnail URL', 'Created At', 'EndCursor']
+    fieldnames = ['DocumentID', 'Name', 'Tagline','Description','Category','Free Plan','Paid Plan','Website','Image URL']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     # ヘッダーの書き込み
